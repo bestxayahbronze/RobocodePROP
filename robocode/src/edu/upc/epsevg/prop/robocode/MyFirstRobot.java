9a5aacbf59438f03df669b041e2d9f2f4df0eba3
Pau Alcázar <pau.alcazar11@gmail.com>
2023-10-10 13:24:18 +0200

    Eliminar comentaris predeterminats del netbeans

--- robocode/src/edu/upc/epsevg/prop/robocode/MyFirstRobot.java
+++ robocode/src/edu/upc/epsevg/prop/robocode/MyFirstRobot.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package edu.upc.epsevg.prop.robocode;
 
 import robocode.AdvancedRobot;
@@ -10,10 +6,6 @@ import robocode.Rules;
 import robocode.ScannedRobotEvent;
 import robocode.TeamRobot;
 
-/**
- *
- * @author e9977173
- */
 public class MyFirstRobot extends TeamRobot{
     
     @Override
